d88651f6442f6651b0a0989430fcdd0967a3e3b8
BLADE <abdirahman.ahmed@student.moringaschool.com>
2021-08-02 11:56:09 +0300

    create account class with it's initializer

---
+++ pw.py
@@ -0,0 +1,99 @@
+import random
+import string 
+
+class Credentials:
+    """Class to generate credentials for different accounts.
+    """
+    accounts_list = []
+    user_accounts = []
+    def __init__(self, acount_name, email, username, password, user):
+        """__init__ method to help define the propertiesof our credentials objects.
+
+        Args:
+          account_name(string): New name of the credential account.
+          email(string): New email address associated with that credentials.
+          username(string):New username for that credential
+          password(string):New password.
+          user([string]): Current logged in user.
+        """
+        self.account_name = account_name
+        self.email = email
+        self.username = username
+        self.password = password
+        self.user = user
+
+
+    def save_account(self):
+        """self_account method saves credential objects into accounts_list
+        """
+        Credentials.accounts_list.append(self)
+
+    @classmethod
+    def generate_pw(cls, pw_length):
+        """generate_pw method that generates random password.
+        
+        Agrs:
+          pw_length (number): Length of generated password.
+
+        Return:
+           string: Return the generated password. 
+        """
+        pw: ''.join(random.choice(string.ascii_letter)for i in range(pw_length))
+        return pw
+
+    @classmethod
+    def set_pw(cls, pw):
+        """set_pw method that sets a given password.
+
+        Args:
+          pw (string): User password to be set.
+
+        Returns:
+          string: Password.
+        """
+        return pw
+
+    @classmethod
+    def dispaly_accounts(cls, pw):
+        """"display_accounts taht loops through the accounts list and filters only those that belong to the current logged in user.
+
+        Args:
+          user (User): Current user.
+
+        Returns: 
+           Credentials: List of filtered credentials.
+        """
+        for account in cls.accounts_list:
+            if accont.user == user:
+                cls.user_accounts.append(account)
+        return cls.user_accounts
+
+    def delete_account(self):
+        """delete_account method taht removes a saved credential from the credentials list.
+        """
+        Credentials.accounts_list.remove(self)                           
+
+class User:
+    """Class to manage user accounts.
+    """
+
+    user_list = []
+    def __init__(self, fname, lname, username, password):
+        """__init__method to generate credentials objects.
+        
+        Args:
+          fname (string): New user first name.
+          lname (string): New user last name.
+          username (string): New user usernmae.
+          password (string): New user password.
+        """
+        self.fname = fname
+        self.lname = lname
+        self.username = username
+        self.password = password  
+          
+
+    
+    
+
+    
